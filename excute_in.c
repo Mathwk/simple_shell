019420039e23ce9b2d3def3587eac8c3d50017b4
mathewos <mathewos1@yahoo.com>
2023-04-26 16:29:11 -0700

    fork added

--- excute_in.c
+++ excute_in.c
@@ -7,19 +7,10 @@
  */
 void excute_in(char **argv)
 {
-	int pd;
 
 	if (argv != NULL)
 	{
-		pd = fork();
-		if (pd == -1)
+		if (execve(full_path(argv[0]), argv, NULL) == -1)
 			perror("(incorrect input) ");
-		if (pd == 0)
-		{
-			if (execve(full_path(argv[0]), argv, NULL) == -1)
-				perror("(incorrect input) ");
-		}
-		else
-			return;
 	}
 }
